75db8dbd2ea904d6b110b30c8ba8eaba19d96d29
fendeee <3011416498@qq.com>
2022-09-25 17:54:31 +0800

    master

--- 7. Vue2-Vue3/hua_wei/src/App.vue
+++ 7. Vue2-Vue3/hua_wei/src/App.vue
@@ -1,15 +1,20 @@
 <template>
-  <div id="app">
-    <Todo></Todo>
-  </div>
+    <div id="app">
+        <Todo></Todo>
+    </div>
 </template>
 
 <script>
 import Todo from './components/TodoList.vue'
 export default {
-  name: 'App',
-  components: {
-    Todo,
-  }
+    name: 'App',
+    components: {
+        Todo,
+    }
 }
 </script>
+
+<style lang="less">
+    * {margin: 0; padding: 0; box-sizing: border-box; }
+    body { background-color: #333; }
+</style>
--- 7. Vue2-Vue3/hua_wei/src/components/TodoList.vue
+++ 7. Vue2-Vue3/hua_wei/src/components/TodoList.vue
@@ -1,33 +1,74 @@
 <template>
-    <div class="Todo">5645</div>
+    <div class="Todo">
+        <div class="nav">
+            <input @click="Write" type="text" placeholder="请输入您的任务名称，按回车键确认...">
+            <div class="checkbox">
+                <ul></ul>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'TodoList',
-  data(){
-    return {}
-  },
-  props: {
-    msg: String
-  }
+    name: 'TodoList',
+    data(){
+        return {
+        }
+    },
+    methods: {
+    },
+    props: {
+        msg: String
+    }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+    div.Todo {
+        position: relative;
+        position: absolute;
+        top: 50%; left: 50%;
+        transform: translate(-50%, -50%);
+        width: 500px; height: 300px;
+        background-color: #d1af82;
+        border-radius: 10px;
+        box-shadow: 2px 2px 15px 2px #8a7457;
+        overflow: hidden;
+        .nav {
+            position: absolute;
+            top: 50%; left: 50%;
+            transform: translate(-50%, -50%);
+            width: 93%; height: 85%;
+            background-color: #faebd7;
+            border-radius: 10px;
+            overflow: hidden;
+            input[type='text']{
+                padding-left: 20px;
+                width: 100%; height: 35px;
+                border-radius: 10px 10px 0 0;
+                border: 1px #999 solid;
+                font-size: 14px;
+                letter-spacing: 1.5px;
+                outline: none;
+            }
+            div.checkbox {
+                margin: 7px auto;
+                width: 97%; height: 67%;
+                border-radius: 10px;
+                border: 1px #999 solid;
+                ul {
+                    width: 100%; height: 100%;
+                }
+            }
+        }
+    }
+    div.Todo::before {
+        content: '';
+        position: absolute;
+        width: 100%; height: 5px;
+        background-color: #c08c48;
+        box-shadow: 0px 0px 12px 1px #211402;
+    }
 </style>
